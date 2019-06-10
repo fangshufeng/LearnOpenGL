f353703891a5e5204ef624c43a02913f8db3541e
fangshufeng <fangsh@dianrong.com>
2019-06-10 18:11:37 +0800

    线性深度

--- Glitter/Sources/08-深度测试/depth.fs
+++ Glitter/Sources/08-深度测试/depth.fs
@@ -1,4 +1,5 @@
 
+// 线性关系请打开下面的注释
 #version 330 core
 
 in vec2 TexCoord;
@@ -11,3 +12,22 @@ void main() {
 //    FragColor = vec4(texture(texture1, TexCoord).rgb,1.0f);
     FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
 }
+
+//线性关系
+//#version 330 core
+//out vec4 FragColor;
+//
+//float near = 0.1;
+//float far  = 100.0;
+//
+//float LinearizeDepth(float depth)
+//{
+//    float z = depth * 2.0 - 1.0; // back to NDC
+//    return (2.0 * near * far) / (far + near - z * (far - near));
+//}
+//
+//void main()
+//{
+//    float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 far
+//    FragColor = vec4(vec3(depth), 1.0);
+//}
